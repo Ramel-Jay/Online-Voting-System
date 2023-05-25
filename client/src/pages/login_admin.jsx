e2c0d3e49d6e1afe64c2a88eac97b08ab9567aab
Ramel Jay <rameljey15@gmail.com>
2023-05-25 13:55:37 +0800

    add tailwindcss for fast to design

---
+++ client/src/pages/login_admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function login_admin() {
+    return (
+        <div>
+            <h1>This is the log in page for admin</h1>
+        </div>
+    )
+}
+
+export default login_admin
