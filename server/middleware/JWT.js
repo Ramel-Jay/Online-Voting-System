de1407b63d9a02b1aeba3220ef4dd262b87e2429
Ramel Jay <rameljey15@gmail.com>
2023-06-05 15:34:36 +0800

    asdsad

---
+++ server/middleware/JWT.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+const createToken = (user) => {
+    const accessToken = jwt.sign({email: user.email, id: user.id, avatar_url: user.avatar_url}, "Secret12345Token");
+    return accessToken;
+}
+
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies["token"];
+
+    if(accessToken){
+        try{
+            const validToken = jwt.verify(accessToken, "Secret12345Token");
+            req.user = validToken;
+
+            if(validToken){
+                console.log("Admin Authenticated");
+                req.authenticated = true;
+                return next();
+            }
+        }catch(err) {
+            return res.json({error: err});
+        }
+    }else {
+        return res.json({error: "Admin not Authenticated"});
+    }
+}
+
+const removeToken = (req, res) => {
+    res.clearCookie("token");
+    res.json("Log out");
+}
+
+module.exports = { createToken, validateToken, removeToken };
